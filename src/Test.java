336af85e3c4185375cffb0c1627ab5510520103a
mambetkerimov <mdaniyar1995@gmail.com>
2022-10-31 15:37:55 +0300

    wff

--- src/Test.java
+++ src/Test.java
@@ -7,8 +7,9 @@ public class Test extends Thread {
     }
 
     public static void main(String[] args) {
-        Test test = new Test();
-        test.run();
+Test test = new Test();
+        test.start();
+
     }
 }
 
