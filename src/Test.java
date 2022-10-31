a74efcf0b6e8288ece1b5a1c52f235a34fe66874
mambetkerimov <mdaniyar1995@gmail.com>
2022-10-31 15:22:37 +0300

    ,

---
+++ src/Test.java
@@ -0,0 +1,54 @@
+public class Test extends Thread {
+
+//    public void run() {
+//        for (int i = 1; i <3 ; i++) {
+//            System.out.println(i + "..");
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Test test = new Test();
+//
+////        test.start();
+//        test.run();
+//    }
+
+
+
+}
+
+//    public static void main(String[] args) {
+//        try {
+//            System.out.println("1");
+//           int a = 2 / 0;
+//            System.out.println("2");
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("3");
+//        } catch (NullPointerException e ) {
+//            System.out.println("4");
+//        } catch (Exception e) {
+//            System.out.println("5");
+//        } finally {
+//            System.out.println("6");
+//        }
+//    }
+//}
+
+
+//    public void run() {
+//        System.out.print(".bfdbd.");
+//    }
+//
+//    public static void main(String[] args) {
+//        Test test = new Test();
+//        test.start();
+//        System.out.print("1few");
+//        test.start();
+//        System.out.print("vdsbv");
+//    }
+//}
+
+
+
+
+
---
+++ src/Test2.java
@@ -0,0 +1,29 @@
+public class Test2 implements Runnable {
+//
+//    @Override
+//    public void run() {
+//        new Thread(new Runnable()).start();
+//    }
+//}
+
+
+//    private int x = 0;
+//    private int y = 0;
+//
+//    public static void main(String[] args) {
+//        Test2 test2 = new Test2();
+//        (new Thread(test2)).start();
+//        (new Thread(test2)).start();
+//    }
+//
+//    public synchronized  void run() {
+//        for (int i = 0; i < 10; i++) {
+//            x++;
+//            y++;
+//            System.out.println("x= " + x + "; y = " + y);
+//        }
+//    }
+//}
+
+
+
